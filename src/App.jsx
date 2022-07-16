76dd5b65d3cf8e35b3bd1bf0dd66b420a312d57c
UntoldGam <charliebird373@gmail.com>
2022-07-16 20:31:19 +0100

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Titles } from './Header/Titles.jsx';
 export class App extends React.Component {
-  render() {
-    return (
+	render() {
+		return (
 			<Titles />
-  	)
+		)
 	}
 }
--- src/Header/Titles.jsx
+++ src/Header/Titles.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './Header.css';
+import 'handsontable/dist/handsontable.full.css';
 
 export class Titles extends React.Component {
 	render() {
 		return (
-			<div id="Titles">
-					<h1 style={{ color: 'white'}}>This is where a title goes! <br /> <small>This is where as subtitle goes</small> </h1>
-					<p> style={{ textSize: "15px" }}> This is where the subtitle goes</p>
-			</div>
-			
+			<>
+				<div id="Titles">
+					<h1 style={{ color: 'white' }}>This is where a title goes!</h1>
+					<p style={{ textSize: "15px" }}> This is where the subtitle goes</p>
+				</div>
+				
+			</>
+
+
 		)
 	}
 }
